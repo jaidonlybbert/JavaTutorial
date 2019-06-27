f0fd208a96cbcc6be2549ab2e4b1a7171d0f55c7
jaidonlybbert <jaidonlybbert@gmail.com>
2019-06-27 16:35:40 -0700

    Basic Syntax Tutorial from tutorialspoint.com

---
+++ basicSyntax/basicSyntax.java
@@ -0,0 +1,9 @@
+// Jaidon Lybbert
+// June 27, 2019
+// From tutorialspoint.com
+
+public class basicSyntax {
+  public static void main(String []args) {
+    System.out.println("Hello World");
+  }
+}
